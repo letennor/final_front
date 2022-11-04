3e3b63a954860cccf9ad339e1cd3b70248dc62b6
letennor <h1229523967@163.com>
2022-11-04 17:53:58 +0800

    初始化一些东西

--- src/main.js
+++ src/main.js
@@ -1,8 +1,15 @@
 import Vue from 'vue'
 import App from './App.vue'
+import element from 'element-ui'
+import router from '@/router/index'
+import store from '@/store/index'
+
 
 Vue.config.productionTip = false
+Vue.use(element)
 
 new Vue({
   render: h => h(App),
+  router,
+  store
 }).$mount('#app')
