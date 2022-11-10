328671c80a2bbbf1f53c58392fa2875455a7cf3e
letennor <h1229523967@163.com>
2022-11-10 10:20:59 +0800

    设置框架样式

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,20 @@
 <template>
   <div id="app">
-    hello 
+    测试elementUI
+    <el-row>
+      <el-button>默认按钮</el-button>
+      <el-button type="primary">主要按钮</el-button>
+      <el-button type="success">成功按钮</el-button>
+      <el-button type="info">信息按钮</el-button>
+      <el-button type="warning">警告按钮</el-button>
+      <el-button type="danger">危险按钮</el-button>
+    </el-row>
   </div>
 </template>
 
 <script>
-
 export default {
-  name: 'App',
-}
+  name: "App",
+};
 </script>
 
--- src/main.js
+++ src/main.js
@@ -1,12 +1,14 @@
 import Vue from 'vue'
 import App from './App.vue'
-import element from 'element-ui'
+import Element from 'element-ui'
 import router from '@/router'
 import store from '@/store'
+import 'element-ui/lib/theme-chalk/index.css'
+import '@/styles/index.scss' // 全局CSS
 
 
 Vue.config.productionTip = false
-Vue.use(element)
+Vue.use(Element)
 
 new Vue({
   render: h => h(App),
